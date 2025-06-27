1ff01b5021b8342d32e727c66104e47c25eedd70
Michael Beck <michaelbeck1206@gmail.com>
2025-06-27 14:49:34 +1200

    About Us page drafted and linked from Header

--- client/routes.tsx
+++ client/routes.tsx
@@ -4,11 +4,13 @@ import Layout from './components/Layout.tsx'
 import Homepage from './components/Homepage.tsx'
 import ProductPurchase from './pages/ProductPurchase.tsx'
 import Registration from './pages/Registration.tsx'
+import AboutUs from './pages/AboutUs.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<Layout />}>
     <Route index element={<Homepage />} />
     <Route path="/shop/:name" element={<ProductPurchase />} />
     <Route path="/register" element={<Registration />} />
+    <Route path="/aboutus" element={<AboutUs />} />
   </Route>,
 )
