aedff335c0974e1e519069244572e252b5858256
Michael Beck <michaelbeck1206@gmail.com>
2025-07-01 16:36:36 +1200

    Features tab done

---
+++ client/components/HFTWatch/Battery.tsx
@@ -0,0 +1,23 @@
+function Battery() {
+  return (
+    <svg
+      fill="currentColor"
+      viewBox="0 0 22 22"
+      xmlns="http://www.w3.org/2000/svg"
+      id="memory-battery-100"
+      className="h-24 w-24"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path d="M5 8H7V14H5V8M8 8H10V14H8V8M18 5V6H19V9H20V13H19V16H18V17H3V16H2V6H3V5H18M17 7H4V15H17V7M11 8H13V14H11V8M14 8H16V14H14V8Z"></path>
+      </g>
+    </svg>
+  )
+}
+
+export default Battery
--- client/components/HFTWatch/Title.tsx
+++ client/components/HFTWatch/Title.tsx
@@ -3,25 +3,23 @@ import { useState } from 'react'
 
 function Title() {
   const [show, setShow] = useState(false)
-  const [opacity, setOpacity] = useState(0)
+  const [opacity, setOpacity] = useState('opacity-0')
 
   useFrame((state) => {
     const timeElapsed = state.clock.getElapsedTime()
 
-    if (timeElapsed > 3 && !show && opacity < 100) {
+    if (timeElapsed > 3.5 && !show) {
       setShow(true)
-      setOpacity((prev) => (prev += 1))
+      setOpacity('opacity-100')
     }
   })
 
   return (
     <>
-      <h1
-        className={`mt-16 text-8xl transition-all duration-500 opacity-${opacity}`}
-      >
+      <h1 className={`mt-16 text-8xl transition-all duration-500 ${opacity}`}>
         LuxTech Watch
       </h1>
-      <p className={`text-xl transition-all duration-500 opacity-${opacity}`}>
+      <p className={`text-xl transition-all duration-500 ${opacity}`}>
         The watch for all your needs
       </p>
     </>
