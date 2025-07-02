2de01bcdf0a7b04634bf9ad1703f25f5c200587c
Michael Beck <michaelbeck1206@gmail.com>
2025-07-03 00:37:17 +1200

    About Us Page styled

--- client/pages/ProductPurchase.tsx
+++ client/pages/ProductPurchase.tsx
@@ -80,8 +80,8 @@ function ProductPurchase() {
 
   return (
     <>
-      <div className="size-dvh mx-32 my-16 flex bg-[var(--bg-color)]">
-        <div>
+      <div className="flex h-[84vh] bg-[var(--order-bg)]">
+        <div className="my-16 ml-32">
           <Link to={`/product/${productData.name}`}>
             <h1 className="my-4 text-6xl decoration-from-font hover:underline">
               {productData.name}
@@ -94,45 +94,59 @@ function ProductPurchase() {
             </span>
             <span>&gt;</span>
           </Link>
-          <img
-            src={productData.image}
-            alt={productData.name}
-            className="my-4 max-w-xl"
-          />
-        </div>
-        <div className="m-16">
-          <p className="text-4xl">NZD {productData.price}</p>
-          {productData.stock < 1 ? (
-            <p>Out Of Stock</p>
-          ) : productData.stock > 10 ? (
-            <p>In Stock</p>
-          ) : productData.stock > 1 ? (
-            <p>{productData.stock} Units Left</p>
-          ) : (
-            <p style={{ color: 'red' }}>1 Unit Left</p>
-          )}
-          <label htmlFor="quantity">Quantity</label>
-          <input
-            id="quantity"
-            type="number"
-            value={isOOS ? '' : quantity}
-            onChange={handleChange}
-            disabled={isOOS}
-          />
-          <button
-            disabled={isOOS}
-            onClick={handleBuy}
-            className="m-2 rounded-xl bg-[#424242] px-4 py-2 text-[white] hover:opacity-80 active:opacity-100"
-          >
-            Buy Now
-          </button>
-          <button
-            disabled={isOOS}
-            onClick={handleAdd}
-            className="m-2 rounded-xl bg-[#424242] px-4 py-2 text-[white] hover:opacity-80 active:opacity-100"
-          >
-            Add To Cart
-          </button>
+          <div className="grid grid-cols-2">
+            <div className="flex h-[384px] w-[512px] justify-center rounded-xl border-2 border-solid border-current">
+              <img
+                src={productData.image}
+                alt={productData.name}
+                className="aspect-3/2 max-h-[383px] max-w-[511px] overflow-hidden "
+              />
+            </div>
+            <div className="mx-16 my-8 rounded-3xl bg-[var(--card-bg)] shadow-[inset_0_2px_6px_rgba(66,66,66,0.05)]">
+              <p className="ml-16 mt-16 text-4xl">NZD {productData.price}</p>
+              {productData.stock < 1 ? (
+                <p className="ml-16 text-[#424242]">Out Of Stock</p>
+              ) : productData.stock > 10 ? (
+                <p className="ml-16 text-[#424242]">In Stock</p>
+              ) : productData.stock > 1 ? (
+                <p className="ml-16 text-[#424242]">
+                  {productData.stock} Units Left
+                </p>
+              ) : (
+                <p className="ml-16 text-[#424242]" style={{ color: 'red' }}>
+                  1 Unit Left
+                </p>
+              )}
+              <br />
+              <label htmlFor="quantity" className="ml-16 text-xs">
+                Quantity
+              </label>
+              <br />
+              <input
+                id="quantity"
+                type="number"
+                value={isOOS ? '' : quantity}
+                onChange={handleChange}
+                disabled={isOOS}
+                className="ml-16 w-24 rounded-md border-2 border-solid border-[black] bg-[var(--text-field)] p-2 text-[black]"
+              />
+              <br />
+              <button
+                disabled={isOOS}
+                onClick={handleBuy}
+                className="my-4 ml-16 rounded-xl bg-[#424242] px-4 py-2 text-[white] hover:opacity-80 active:opacity-100"
+              >
+                Buy Now
+              </button>
+              <button
+                disabled={isOOS}
+                onClick={handleAdd}
+                className="mx-2 my-4 rounded-xl bg-[#424242] px-4 py-2 text-[white] hover:opacity-80 active:opacity-100"
+              >
+                Add To Cart
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
